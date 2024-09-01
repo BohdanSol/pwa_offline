456cd2c102ea6a96a076e7ddec41943f82946fff
Bohdan <bohdan.solovey@coaxsoft.com>
2024-09-01 12:25:27 +0300

    feat: fetch request on open api

--- app.js
+++ app.js
@@ -1,3 +1,32 @@
+const API_URL = "https://jsonplaceholder.typicode.com/posts";
+const dataContainer = document.getElementById("data-container");
+
+async function fetchData() {
+  try {
+    const response = await fetch(API_URL);
+    const data = await response.json();
+    displayData(data);
+  } catch (error) {
+    console.error("Failed to fetch data:", error);
+    dataContainer.textContent = "Failed to load data.";
+  }
+}
+
+function displayData(data) {
+  console.log("data", data);
+  const postsHtml = data
+    .map(
+      (post) => `
+        <div class="post">
+            <h2>${post.title}</h2>
+            <p>${post.body}</p>
+        </div>
+    `
+    )
+    .join("");
+  dataContainer.innerHTML = postsHtml;
+}
+
 if ("serviceWorker" in navigator) {
   navigator.serviceWorker
     .register("/service-worker.js")
@@ -8,3 +37,5 @@ if ("serviceWorker" in navigator) {
       console.error("Service Worker registration failed:", error);
     });
 }
+
+fetchData();
